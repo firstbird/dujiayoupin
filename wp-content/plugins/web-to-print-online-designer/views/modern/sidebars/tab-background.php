051874f9427eb9f46869af1727fac40eaa0d39b4
mzlzjhz <mzlzjhz@163.com>
2025-05-06 00:16:39 +0800

    default bg image

--- wp-content/plugins/web-to-print-online-designer/views/modern/sidebars/tab-background.php
+++ wp-content/plugins/web-to-print-online-designer/views/modern/sidebars/tab-background.php
@@ -1,25 +1,10 @@
 <div class="<?php if( $active_backgrounds ) echo 'active'; ?> tab" ng-if="settings['nbdesigner_enable_image'] == 'yes'" id="tab-photo" nbd-scroll="scrollLoadMore(container, type)" data-container="#tab-photo" data-type="background" data-offset="30">
     <div class="nbd-search">
-        <input ng-class="(resource.personal.status || !resource.photo.onclick) ? 'nbd-disabled' : ''" ng-keyup="$event.keyCode == 13 && getPhoto(resource.photo.type, 'search')" type="text" name="search" placeholder="<?php esc_html_e('Search photo', 'web-to-print-online-designer'); ?>" ng-model="resource.photo.photoSearch"/>
+        <input type="text" name="search" placeholder="<?php esc_html_e('Search background', 'web-to-print-online-designer'); ?>" ng-model="resource.photo.photoSearch"/>
         <i class="icon-nbd icon-nbd-fomat-search"></i>
     </div>
     <div class="tab-main tab-scroll">
-        <div class="nbd-items-dropdown">
-            <div class="main-items">
-                <div class="items">
-                    <div class="item" ng-click="onClickTab('upload', 'photo')" ng-if="settings['nbdesigner_enable_upload_image'] == 'yes'" data-type="image-upload" data-api="false">
-                        <div class="main-item">
-                            <div class="item-icon"><i class="icon-nbd icon-nbd-file-upload"></i></div>
-                            <div class="item-info">
-                                <span class="item-name" title="Image upload"><?php esc_html_e('Upload','web-to-print-online-designer'); ?></span>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <?php do_action('nbd_modern_sidebar_photo_icons'); ?>
-                </div>
-                <div class="pointer"></div>
-            </div>
+            
             <div class="result-loaded">
                 <div class="content-items">
                     <div ng-class="settings['nbdesigner_upload_show_term'] !== 'yes' ? 'accept' : '' " class="content-item type-upload" data-type="image-upload">
@@ -45,28 +30,41 @@
                                 <span><?php esc_html_e('Max file size','web-to-print-online-designer'); ?>: <strong>{{settings['nbdesigner_maxsize_upload']}} MB</strong></span>
                                 <span><?php esc_html_e('Min file size','web-to-print-online-designer'); ?>: <strong>{{settings['nbdesigner_minsize_upload']}} MB</strong></span>
                             </div>
-                            <div class="nbd-term" ng-if="settings['nbdesigner_upload_show_term'] == 'yes'">
+                            <div class="clear-local-images-wrap" style="margin-left: 12px;">
+                                <button type="button" class="nbd-clear-btn" ng-click="clearBackgroundFiles()">
+                                    <?php esc_html_e('清除所有图片','web-to-print-online-designer'); ?>
+                                </button>
+                            </div>
+                                <div class="nbd-term" ng-if="settings['nbdesigner_upload_show_term'] == 'yes'">
                                 <div class="nbd-checkbox">
                                     <input id="accept-term" type="checkbox">
                                     <label for="accept-term">&nbsp;</label>
                                 </div>
                                 <span class="term-read"><?php esc_html_e('I accept the terms','web-to-print-online-designer'); ?></span>
                             </div>
-                            <div id="nbd-upload-wrap">
+                            <div id="nbd-upload-wrap" ng-show="resource.upload.data.length > 0">
                                 <div class="mansory-wrap">
                                     <div nbd-drag="img.url" nbd-img="img" extenal="false" type="image" class="mansory-item" ng-click="resource.addImageContext = 'manual'; setBackgroundUrl(img.url);" ng-repeat="img in resource.upload.data track by $index" repeat-end="onEndRepeat('upload')"><img ng-src="{{img.url}}"></div>
                                 </div>
                             </div>
-                            <div class="clear-local-images-wrap" ><span ng-click="_localStorage.delete('nbduploaded')"><?php esc_html_e('Clear all uploaded images','web-to-print-online-designer'); ?></span></div>
                         </div>
                     </div>
                     
                     <?php do_action('nbd_modern_sidebar_photo_images'); ?>
                 </div>
                 <div class="nbdesigner-gallery" id="nbdesigner-gallery">
-                    <div nbd-drag="img.url" extenal="true" type="image" class="nbdesigner-item" ng-click="resource.addImageContext = 'manual'; setBackgroundUrl(img.url)" ng-repeat="img in resource.photo.data" repeat-end="onEndRepeat('photo')"><img ng-src="{{img.preview}}"><span class="photo-desc">{{img.des}}</span></div>
+                    <!-- 没有图片时显示默认图片 -->
+                    <div ng-if="!resource.photo.data || (resource.photo.data && resource.photo.data.length == 0)" class="nbdesigner-default-image">
+                        <img src="https://www.dujiayoupin.com/wp-content/uploads/2025/01/placeholder-289.png" alt="暂无图片" style="width: 120px; opacity: 0.5;">
+                        <div style="color: #999; margin-top: 8px;">暂无图片</div>
+                    </div>
+                    <!-- 有图片时正常显示 -->
+                    <div nbd-drag="img.url" extenal="true" type="image" class="nbdesigner-item" ng-click="resource.addImageContext = 'manual'; setBackgroundUrl(img.url)" ng-repeat="img in resource.photo.data" repeat-end="onEndRepeat('photo')">
+                        <img ng-src="{{img.preview}}">
+                        <span class="photo-desc">{{img.des}}</span>
+                    </div>                
                 </div>
-                <div class="loading-photo" >
+                <div class="loading-photo" ng-show="false">
                     <svg class="circular" viewBox="25 25 50 50">
                         <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
                     </svg>
@@ -75,6 +73,5 @@
                     <a class="nbd-button" ng-click="scrollLoadMore('#tab-photo', 'photo')"><?php esc_html_e('Load more','web-to-print-online-designer'); ?></a>
                 </div>
             </div>
-        </div>
     </div>
 </div>
